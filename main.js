4b4506caf7a088959fea96f45a1593ddec527dbb
Jay OConnor <jdoconnor@hotmail.com>
2017-07-01 08:07:45 -0700

    initial version

---
+++ main.js
@@ -0,0 +1,60 @@
+const electron = require('electron')
+const { spawn } = require('child_process')
+// Module to control application life.
+const app = electron.app
+
+const path = require('path')
+const url = require('url')
+const ipc = electron.ipcMain
+const Menu = electron.Menu
+const Tray = electron.Tray
+
+const getContextCmd = spawn("kubectl", ["config", "view", "-o", "json"]).on('error', function( err ){ throw err })
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let appIcon
+
+async function createMenuBar () {
+  var kubecfg = await getKubeConfig();
+  console.log(kubecfg)
+  const iconName = 'menubar-icon.png'
+  const iconPath = path.join(__dirname, iconName)
+  appIcon = new Tray(iconPath)
+  menuTemplate<Hash> = []
+  for(context of kubecfg.contexts){
+    menuTemplate.push({
+      label: context.name,
+      click: function() {
+        app.quit()
+      }
+    })
+  }
+  // add the quit menuitem
+  menuTemplate.push({
+    label: 'Quit',
+    click: function () {
+      app.quit()
+    }
+  })
+  
+  contextMenu = Menu.buildFromTemplate(menuTemplate)
+  
+  appIcon.setContextMenu(contextMenu)
+}
+
+function getKubeConfig() {
+  return new Promise(resolve => {
+    getContextCmd.stdout.on('data', (data) => {
+      resolve(JSON.parse(data.toString()))
+    });
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createMenuBar)
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
