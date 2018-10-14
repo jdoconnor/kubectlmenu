1e5d3fb92b882e200bc9e6232041706c01a875c0
Jay <jdoc@Jays-MBP.hsd1.ca.comcast.net>
2018-10-14 11:30:36 -0700

    bitbar formatting

--- crystal/src/kubectlmenu.cr
+++ crystal/src/kubectlmenu.cr
@@ -5,8 +5,6 @@ module Kubectlmenu
   VERSION = "0.1.0"
 
   string = `kubectl get pods --all-namespaces -o=json`
-  puts "done with kubectl"
-  # namespaces = Kubernetes::Api::V1::NamespaceList
   namespaces = {} of String => Array(Pyrite::Api::Core::V1::Pod)
   Pyrite::Api::Core::V1::List.from_json(string).items.not_nil!.select do |pod|
     if pod.is_a?(Pyrite::Api::Core::V1::Pod)
@@ -17,13 +15,10 @@ module Kubectlmenu
       namespaces[namespace_name] << pod
     end
   end
-  puts namespaces.size
-  puts "==="
-  namespaces.each do |k,v|
-    puts k
-    puts "--"
-    v.each do |p|
-      puts p.metadata.try(&.name)
+  namespaces.each do |name,pods|
+    puts name
+    pods.each do |p|
+      puts "-- #{p.metadata.try(&.name)}"
     end
   end
 end
